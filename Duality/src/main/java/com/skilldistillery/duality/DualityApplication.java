d9afe2b6bd429433cab960e23026c1ff42b9f649
Justin Martz <justinmartz@protonmail.com>
2023-08-22 16:18:04 -0600

    Spring Boot project created, configured project dependency

---
+++ Duality/src/main/java/com/skilldistillery/duality/DualityApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.duality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DualityApplication.class, args);
+	}
+
+}
---
+++ Duality/src/test/java/com/skilldistillery/duality/DualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.duality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
