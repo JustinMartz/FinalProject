8f4d33d06b145c4746c76c004ddd9761d723034e
Jay Perkins <jul3jb@gmail.com>
2023-08-23 09:07:03 -0700

    Create Angular front end project

---
+++ ngDuality/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'ngDuality';
+
+  constructor(private auth: AuthService){}
+
+  ngOnInit() {
+    this.tempTestDeleteMeLater(); // DELETE LATER!!!
+  }
+
+  tempTestDeleteMeLater() {
+    this.auth.login('admin','test').subscribe({ // change username to match DB
+      next: (data) => {
+        console.log('Logged in:');
+        console.log(data);
+      },
+      error: (fail) => {
+        console.error('Error authenticating:')
+        console.error(fail);
+      }
+    });
+  }
+
+}
---
+++ ngDuality/src/app/services/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import{User} from '../models/user'
+import { Buffer } from 'buffer';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+}
