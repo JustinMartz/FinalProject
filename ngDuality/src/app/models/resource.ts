58f74aa7a15f4f750c4e01a8ab4f454388cfd510
fathsam82 <fathsam82@gmail.com>
2023-08-25 11:47:18 -0600

    models for user and resource

---
+++ ngDuality/src/app/models/resource.ts
@@ -0,0 +1,42 @@
+import { User } from "./user";
+
+export class Resource {
+  id: number;
+  resourceLink: string;
+  description: string;
+  createDate: string;
+  active: boolean;
+  creator: User;
+
+
+
+
+  constructor(
+    id: number = 0,
+    resourceLink: string = '',
+    description: string = '',
+    createDate: string = '',
+    active: boolean = false,
+    creator: User = new User()
+
+
+
+
+
+
+
+
+
+  ) {
+    this.id = id;
+    this.resourceLink = resourceLink;
+    this.description = description;
+    this.createDate = createDate;
+    this.active = active;
+    this.creator = creator;
+
+
+
+  }
+
+}
--- ngDuality/src/app/models/user.ts
+++ ngDuality/src/app/models/user.ts
@@ -1,3 +1,8 @@
+import { first } from "rxjs";
+import { BehaviorReport } from "./behavior-report";
+import { BehaviorReportRemark } from "./behavior-report-remark";
+import { Resource } from "./resource";
+
 export class User {
   id: number;
   username: string;
@@ -5,6 +10,19 @@ export class User {
   email: string;
   role: string;
   enabled: boolean;
+  avatar: string;
+  firstName: string;
+  lastName: string;
+  createDate: string;
+  udpateDate: string;
+  dob: string;
+  aboutMe: string;
+  behaviorReports: BehaviorReport[] | undefined;
+  behaviorReportRemarks: BehaviorReportRemark[] | undefined;
+  resources: Resource [] | undefined;
+  flaggedPosts: Post [] | undefined;
+  posts: Post [] | undefined;
+
 
   constructor(
     id: number = 0,
@@ -12,7 +30,23 @@ export class User {
     password: string = '',
     email: string = '',
     role: string = '',
-    enabled: boolean = false
+    enabled: boolean = false,
+    avatar: string = '',
+    firstName: string = '',
+    lastName: string = '',
+    createDate: string = '',
+    updateDate: string = '',
+    dob: string = '',
+    aboutMe: string = '',
+    behaviorReports: BehaviorReport [] = [],
+    behaviorReportRemarks: BehaviorReportRemark [] = [],
+    resources: Resource [] = [],
+    flaggedPosts: Post[] | undefined,
+    posts: Post[] | undefined
+
+
+
+
   ) {
     this.id = id;
     this.username = username;
@@ -20,6 +54,18 @@ export class User {
     this.email = email;
     this.role = role;
     this.enabled = enabled;
+    this.avatar = avatar;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.createDate = createDate;
+    this.udpateDate = updateDate;
+    this.dob = dob;
+    this.aboutMe = aboutMe;
+    this.behaviorReports = behaviorReports;
+    this.behaviorReportRemarks = behaviorReportRemarks;
+    this.resources = resources;
+    this.flaggedPosts = flaggedPosts;
+    this.posts = posts;
   }
 
 }
