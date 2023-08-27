cbb7e275772b78fd983627025da5c2492f558406
Alex A <Alexapakidze@yahoo.com>
2023-08-27 13:44:14 -0600

    fixed behavior controller merge issue

--- Duality/src/main/java/com/skilldistillery/duality/controllers/BehaviorController.java
+++ Duality/src/main/java/com/skilldistillery/duality/controllers/BehaviorController.java
@@ -2,10 +2,8 @@ package com.skilldistillery.duality.controllers;
 
 import java.security.Principal;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.skilldistillery.duality.entities.Behavior;
@@ -21,32 +20,41 @@ import com.skilldistillery.duality.services.BehaviorService;
 
 @RestController
 @CrossOrigin({ "*", "http://localhost/" })
+@RequestMapping("api")
 public class BehaviorController {
 
 	@Autowired
 	private BehaviorService behaviorService;
 
 	@GetMapping("behaviors")
-	List<Behavior> listBehavior( HttpServletResponse res) {
+	List<Behavior> listBehavior(HttpServletResponse res) {
 		List<Behavior> behaviors = null;
 		behaviors = behaviorService.listAllBehaviors();
 
 		if (behaviors == null) {
 			res.setStatus(404);
+		} else {
+			res.setStatus(200);
 		}
 
 		return behaviors;
 	}
 
 	@GetMapping("behaviors/{id}")
-	Behavior getBehaviorById(@PathVariable("id") int behaviorId, Principal principal, HttpServletResponse res) {
+	Behavior getBehaviorById(@PathVariable("id") int behaviorId,  HttpServletResponse res) {
 		Behavior behavior = behaviorService.getById(behaviorId);
-		if (principal.getName() != null) {
-			if (behavior == null) {
-				res.setStatus(404);
-			}
+		if (behavior == null) {
+			res.setStatus(404);
+		} else {
+			res.setStatus(200);
 		}
+
 		return behavior;
 	}
+
+	
+
+	
+
 
 }
