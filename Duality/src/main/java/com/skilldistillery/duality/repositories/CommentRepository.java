a47dea0093efa2fc551709c173b5170f7ae2b554
fathsam82 <fathsam82@gmail.com>
2023-08-29 14:57:40 -0600

    git branching issue

--- Duality/src/main/java/com/skilldistillery/duality/repositories/CommentRepository.java
+++ Duality/src/main/java/com/skilldistillery/duality/repositories/CommentRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.skilldistillery.duality.entities.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-   
-	
+
 }
