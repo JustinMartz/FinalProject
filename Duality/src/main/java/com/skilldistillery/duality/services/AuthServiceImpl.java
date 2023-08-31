9820ccbf003bea7c5e9c917d9fad70f1b120366c
Justin Martz <justinmartz@protonmail.com>
2023-08-31 09:12:27 -0600

    Fixed AuthServiceImpl to null check and set all user properties

--- Duality/src/main/java/com/skilldistillery/duality/services/AuthServiceImpl.java
+++ Duality/src/main/java/com/skilldistillery/duality/services/AuthServiceImpl.java
@@ -1,6 +1,5 @@
 package com.skilldistillery.duality.services;
 
-import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -23,11 +22,37 @@ public class AuthServiceImpl implements AuthService {
 	public User register(User user) {
 		User newUser = new User();
 		String encrypted = encoder.encode(user.getPassword());
-		newUser.setUsername(user.getUsername());
+		
+		if (userRepo.findByUsername(user.getUsername()) != null) {
+			return null;
+		} else {
+			newUser.setUsername(user.getUsername());
+		}
+		
 		newUser.setPassword(encrypted);
 		newUser.setEnabled(true);
 		newUser.setRole("user");
-		user.setPassword(encrypted);
+		
+		if (user.getEmail() != null) {
+			newUser.setEmail(user.getEmail());		
+		}
+		
+		if (user.getAvatar() != null) {
+			newUser.setAvatar(user.getAvatar());			
+		}
+		
+		if (user.getFirstName() != null) {
+			newUser.setFirstName(user.getFirstName());
+		}
+		
+		if (user.getLastName() != null) {
+			newUser.setLastName(user.getLastName());
+		}
+		
+		if (user.getDob() != null) {
+			newUser.setDob(user.getDob());
+		}
+		
 		userRepo.saveAndFlush(newUser);
 		return user;
 	}
